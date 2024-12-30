44907b90a89b8b6084bf9de9d8ad367a74502999
qgao21 <38676078+qgao21@users.noreply.github.com>
2024-12-30 14:52:31 +0800

    Update train_DGDiff_stage2.sh

--- DGDiff/train_DGDiff_stage2.sh
+++ DGDiff/train_DGDiff_stage2.sh
@@ -1,19 +1,8 @@
-#!/bin/bash
-
-# 第一次运行 test.py
-CUDA_VISIBLE_DEVICES=3 python test.py \
-  --mode test\
-  --test_dataset mayo2016 \
-  --dose 2 \
-  --image_size 256 \
-  --sampling_timesteps 1000
-
-# 第二次运行 test.py
-CUDA_VISIBLE_DEVICES=3 python test.py \
-  --mode test\
-  --test_dataset mayo2016 \
-  --dose 4 \
-  --cond_size 256 \
+CUDA_VISIBLE_DEVICES=gpu_id python main.py \
+  --mode train\
+  --dataset mayo2016 \
+  --dose 0 \
   --image_size 512 \
-  --sampling_timesteps 30 \
+  --cond_size 256
+  --sampling_steps 30\
   --use_cond
