26f2f757529dd2bac58a6ad044aa25ed3b3aa563
qgao21 <38676078+qgao21@users.noreply.github.com>
2024-12-30 14:46:32 +0800

    Update test_DGDiff.sh

--- DGDiff/test_DGDiff.sh
+++ DGDiff/test_DGDiff.sh
@@ -1,7 +1,19 @@
-CUDA_VISIBLE_DEVICES=3 python test.py\
-  --test_dataset mayo2016\
-  --dose 4\
-  --cond_size 256\
-  --image_size 512\
-  --sampling_steps 30\
+#!/bin/bash
+
+# stage 1: image size 256
+CUDA_VISIBLE_DEVICES=gpu_id python main.py \
+  --mode test\
+  --dataset mayo2016 \
+  --dose 2 \
+  --image_size 256 \
+  --sampling_steps 1000
+
+# stage 2: image size 256 -> 512
+CUDA_VISIBLE_DEVICES=gpu_id python main.py \
+  --mode test\
+  --dataset mayo2016 \
+  --dose 2 \
+  --cond_size 256 \
+  --image_size 512 \
+  --sampling_timesteps 30 \
   --use_cond
